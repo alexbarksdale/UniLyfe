3e7ae6e7f26062dfe863836d336d633f6af330dd
alexbarksdale <ale.barksdale@gmail.com>
2020-06-10 01:04:40 -0500

    [Add] deletePost mutation

--- packages/backend/src/resolvers/Post.resolver.ts
+++ packages/backend/src/resolvers/Post.resolver.ts
@@ -1,8 +1,11 @@
+import { createQueryBuilder } from 'typeorm';
 import { Resolver, Query, Mutation, Arg } from 'type-graphql';
 
 import { PostEntity } from '../entity/Post.entity';
 import { UserEntity } from '../entity/User.entity';
+import { logger } from '../utils/logger.utils';
 
+// TODO: Secure the queries and mutations after testing
 @Resolver()
 export class PostResolver {
     @Query(() => PostEntity)
@@ -35,9 +38,29 @@ export class PostResolver {
         try {
             await PostEntity.save(post);
         } catch (err) {
+            logger.error(`Unable to save post! Post content: ${post}\n Error: ${err}`);
             throw new Error(`Unable to save post! ${err}`);
         }
 
         return post;
+    }
+
+    @Mutation(() => Boolean)
+    async deletePost(@Arg('post_id') post_id: number) {
+        if (!post_id) throw new Error('You must provide a post_id!');
+
+        try {
+            const deletedPost = await createQueryBuilder()
+                .delete()
+                .from(PostEntity)
+                .where({ id: post_id })
+                .execute();
+            if (deletedPost.affected) return true;
+        } catch (err) {
+            logger.error(`Failed to delete post with post_id: ${post_id}! Error: ${err}`);
+            throw new Error('Failed to delete post!');
+        }
+
+        return false;
     }
 }
