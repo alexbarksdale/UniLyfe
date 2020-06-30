b68a2e0fb4b503cdc25c6963b3d95b406e829967
alexbarksdale <ale.barksdale@gmail.com>
2020-06-30 14:26:43 -0500

    [Add] Create Post button

---
+++ packages/frontend/src/components/feed/fixed-feed/CreatePost.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const CreateBtn = styled.button`
+    padding: 13px;
+    margin-bottom: 15px;
+    font-size: 15px;
+    font-weight: 500;
+    border-radius: 8px;
+    width: 100%;
+    color: ${(props) => props.theme.white};
+    letter-spacing: 0.7px;
+    background-color: ${(props) => props.theme.primary};
+    transition: all 0.3s ease 0s;
+
+    &:hover {
+        color: ${(props) => props.theme.white};
+        background-color: ${(props) => props.theme.darkPrimary};
+    }
+`;
+
+export function CreatePost(): JSX.Element {
+    return (
+        <Link to='/create'>
+            <CreateBtn>Create Post</CreateBtn>
+        </Link>
+    );
+}
--- packages/frontend/src/components/feed/fixed-feed/FixedFeed.tsx
+++ packages/frontend/src/components/feed/fixed-feed/FixedFeed.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { CreatePost } from './CreatePost';
 import { NewsFeed } from './NewsFeed';
 
 const FixedContainer = styled.div``;
 export function FixedFeed(): JSX.Element {
     return (
         <FixedContainer>
+            <CreatePost />
             <NewsFeed />
         </FixedContainer>
     );
